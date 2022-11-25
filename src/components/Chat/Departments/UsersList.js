89dd5f75438552927b557a837e332585c902fc6b
issdilshod <dilshod@themembers.com>
2022-11-25 16:29:45 +0300

    fix chat list design

--- src/components/Chat/Departments/UsersList.js
+++ src/components/Chat/Departments/UsersList.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Collapse } from "react-bootstrap";
 import { FaComment } from "react-icons/fa";
 import ReactTimeAgo from 'react-time-ago';
 
@@ -7,7 +8,7 @@ const UsersList = ({handleClick, department}) => {
     const [usersShow, setUsersShow] = useState(false);
 
     return (
-        <div className='c-item'>
+        <div className='c-item mt-2'>
             <div className='d-flex'>
                 <div className='mr-auto d-cursor-pointer ' onClick={ () => { setUsersShow(!usersShow) } }>{department['department_name']}</div>
                 <div>
@@ -19,32 +20,36 @@ const UsersList = ({handleClick, department}) => {
                 </div>
             </div>
 
-            <div className={`c-item-under-list ${usersShow?'c-item-under-list-active':''}`}>
-                {   
-                    department['users'].map((value, index) => {
-                        return (
-                            <div key={index} className='c-item-under d-flex'>
-                                <div className='mr-2'><b>{value['first_name'] + ' ' + value['last_name']}</b></div>
-                                <div className='mr-auto'>
-                                    { (value['last_seen']!=null) &&
-                                        <ReactTimeAgo date={new Date(value['last_seen'])} locale="en-US" /> 
-                                    }
-                                    { (value['last_seen']==null) &&
-                                        <span className='online'></span>
-                                    }
+            <Collapse 
+                in={usersShow}
+            >
+                <div className=''>
+                    {   
+                        department['users'].map((value, index) => {
+                            return (
+                                <div key={index} className='c-item-under d-flex'>
+                                    <div className='mr-2'><b>{value['first_name'] + ' ' + value['last_name']}</b></div>
+                                    <div className='mr-auto'>
+                                        { (value['last_seen']!=null) &&
+                                            <ReactTimeAgo date={new Date(value['last_seen'])} locale="en-US" /> 
+                                        }
+                                        { (value['last_seen']==null) &&
+                                            <span className='online'></span>
+                                        }
+                                    </div>
+                                    <div>
+                                        <button className='d-btn d-btn-sm d-btn-primary' onClick={ () => { handleClick(value['uuid'], '~each~') } }>
+                                            <i>
+                                                <FaComment />
+                                            </i>
+                                        </button>
+                                    </div>
                                 </div>
-                                <div>
-                                    <button className='d-btn d-btn-sm d-btn-primary' onClick={ () => { handleClick(value['uuid'], '~each~') } }>
-                                        <i>
-                                            <FaComment />
-                                        </i>
-                                    </button>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
-            </div>
+                            )
+                        })
+                    }
+                </div>
+            </Collapse>
         </div>
     );
 
